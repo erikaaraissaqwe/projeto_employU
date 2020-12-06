a18b0ef00d76799bb0024b5bc80e921039216458
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2020-12-06 17:17:22 -0300

    register

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,10 +1,16 @@
-const Company = require("../models/Company");
 const Candidate = require("../models/Candidate");
 
 module.exports = {
 
     async register(req, res){
-        
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const cpf = req.body.cpf;
+
+        const user = Candidate.create({name, email, password, cpf});
+
+        return res.json({user});
     },
 
 
