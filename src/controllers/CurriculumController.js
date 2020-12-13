a93cc28d18c1134ab1ba8529e5ed5fc711347191
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2020-12-13 17:47:50 -0300

    at

--- src/controllers/CurriculumController.js
+++ src/controllers/CurriculumController.js
@@ -4,15 +4,23 @@ const Curriculum = require("../models/Curriculum");
 module.exports = {
 
     async store(req, res){
+        
         const {fileProfile} = req.files;
-        const { address, professionalExperiences, academicFormations} = req.body;
-        const { user_id } = req.headers;
+        const address = req.body.address;
+        const {professionalExperiences} = req.body;
+        const {academicFormations} = req.body;
+        const {user_id} = req.headers;
 
         const candidate = await Candidate.findById(user_id);
 
         if(!candidate){
             return res.status(400).json({errorMessage : 'não existe esse usuário'});
         }
+        console.log(fileProfile);
+        console.log(address);
+        console.log(user_id);
+        console.log(professionalExperiences);
+        console.log(academicFormations);
 
         const curriculum = await Curriculum.create({
             user : user_id,
